ea1445fae09dbe85df808c89f35d537960136dcb
19aayu <aayushisharma.sr@gmail.com>
2024-01-17 00:23:39 +0530

    Initial commit

--- src/Chip.js
+++ src/Chip.js
@@ -1,22 +1,3 @@
-// // src/Chip.js
-// import React from "react";
-
-// const Chip = ({ label, onClose, highlighted, onChipClick }) => {
-//   return (
-//     <div
-//       className={`chip ${highlighted ? "highlighted" : ""}`}
-//       onClick={() => onChipClick(label)}
-//     >
-//       {label}
-//       <span className="chip-close" onClick={(e) => onClose(label, e)}>
-//         &times;
-//       </span>
-//     </div>
-//   );
-// };
-
-// export default Chip;
-
 // Chip.js
 import React from "react";
 import "./ChipInput.css"; // Adjust the import based on your file structure
